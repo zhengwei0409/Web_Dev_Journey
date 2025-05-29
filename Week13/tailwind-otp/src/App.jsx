f46c0a2b4040c2e8b3249b1542daf782b6b47989
ZhengWei <zhengwei0409@gmail.com>
2025-05-29 22:54:13 +0800

    Updated Week12

---
+++ Week13/tailwind-otp/src/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+import { Otp } from './components/Otp'
+
+function App() {
+  return(
+    <Otp/>
+  )
+}
+
+export default App
---
+++ Week13/tailwind-otp/src/components/Otp.jsx
@@ -0,0 +1,50 @@
+import { useRef } from "react";
+
+
+export function Otp() {
+    const ref = useRef();
+    const ref2 = useRef();
+    const ref3 = useRef();
+    const ref4 = useRef();
+    const ref5 = useRef();
+    const ref6 = useRef();
+
+    return (
+        <>
+        <div className="">
+            <SubOtpBox reference={ref} OnDone={() => {
+                ref2.current.focus();
+            }} />
+            <SubOtpBox reference={ref2} OnDone={() => {
+                ref3.current.focus();
+            }} />
+             <SubOtpBox reference={ref3} OnDone={() => {
+                ref3.current.focus();
+            }} />
+
+        </div>
+            
+            
+        
+        </>
+    )
+    
+
+}
+
+function SubOtpBox({
+    reference,
+    OnDone
+}) {
+    return (
+        <>
+            <input 
+            type="text" 
+            ref={reference} 
+            onChange={OnDone}
+            className="m-1 w-[40px] h-[50px] rounded-xl outline-none px-4 text-white bg-blue-500"
+            />
+
+        </>
+    )
+}
